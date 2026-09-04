0dc1893753db4a8bf8a64d3ddeb8bc39b1461b94
Lady-Ladywaa <kusuma.pkmn@gmail.com>
2026-09-04 14:42:25 +0700

    first commit

---
+++ backend/src/fakeDB/fakeUsers.js
@@ -0,0 +1,19 @@
+export const users = [
+{
+    id: "1",
+    name: "Ladywa",
+    email: "ladywa@example.com",
+    password: "password123",
+},
+{
+    id: "2",
+    name: "Bilal",
+    email: "bilal@example.com",
+    password: "password123",
+},
+{
+    id: "3",
+    name: "Hafiz",
+    email: "hafiz@example.com",
+    password: "password123",}
+]
